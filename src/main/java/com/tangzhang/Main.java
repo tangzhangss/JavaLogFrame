55224967056306c59adb190087a98b9ebe36c873
tangzhang <zhang.tang@cy-tech.net>
2022-08-16 17:29:05 +0800

    init

---
+++ src/main/java/com/tangzhang/Main.java
@@ -0,0 +1,40 @@
+package com.tangzhang;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Integer COUNT = 100;
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            printInfo();
+        }).start();
+
+ /*       new Thread(()->{
+            printWaring();
+        }).start();
+
+        new Thread(()->{
+            printServe();
+        }).start();*/
+
+
+    }
+
+
+    private static void printInfo(){
+        for (int i = 0; i < COUNT; i++) {
+            StaticLog.info(">>>>>info "+i);
+        }
+    }
+    private static void printWaring(){
+        for (int i = 0; i < COUNT; i++) {
+            StaticLog.warning(">>>>>warning "+i);
+        }
+    }
+    private static void printServe(){
+        for (int i = 0; i < COUNT; i++) {
+            StaticLog.severe(">>>>>serve "+i);
+        }
+    }
+}
---
+++ src/main/java/com/tangzhang/StaticLog.java
@@ -0,0 +1,65 @@
+package com.tangzhang;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public final class StaticLog {
+    private StaticLog() {}
+
+    private static Logger logger = Logger.getLogger(StaticLog.class.getPackage().getName());
+
+    static {
+        LogManager logManager = LogManager.getLogManager();
+        try {
+            String userDIR = System.getProperty("user.dir");
+            logManager.readConfiguration(new FileInputStream(userDIR+"/src/main/resources/logger-jul.properties"));
+
+            //判断日志文件目录是否存在--存在就创建目录
+            final String filePattern = logManager.getProperty("java.util.logging.FileHandler.pattern");
+            File logDir =  new File(filePattern).getParentFile();
+            if(!logDir.exists()) {
+                logDir.mkdirs();
+            }
+            //-----------------------------------------
+
+        } catch (IOException e) {
+            //加载日志配置文件失败
+            e.printStackTrace();
+        }
+    }
+
+    public void setLogger(String name){
+        logger = Logger.getLogger(name);
+    }
+
+    public static StaticLog logger(String name){
+        StaticLog staticLog = new StaticLog();
+        staticLog.setLogger(name);
+        return staticLog;
+    }
+
+    public static void info(String msg){
+        logger.info(msg);
+    }
+    public static void severe(String msg){
+        logger.severe(msg);
+    }
+    public static void config(String msg){
+        logger.config(msg);
+    }
+    public static void finer(String msg){
+        logger.finer(msg);
+    }
+    public static void fine(String msg){
+        logger.fine(msg);
+    }
+    public static void finest(String msg){
+        logger.finest(msg);
+    }
+    public static void warning(String msg){
+        logger.warning(msg);
+    }
+}
